5d9f9193d81b4661536dfc31dd7cf1ff205f497b
PoSeiDonTeaM <johnkotsas@me.com>
2018-02-13 22:53:49 +0200

    Button Yes Added

--- app/src/main/java/poseidonteam/valentinesapp/NoPage.java
+++ app/src/main/java/poseidonteam/valentinesapp/NoPage.java
@@ -1,13 +1,34 @@
 package poseidonteam.valentinesapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NoPage extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_no_page);
+
+        button = (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSecondStep();
+            }
+        });
     }
+
+
+    public void openSecondStep() {
+        Intent intent = new Intent(this, Step2.class);
+        startActivity(intent);
+    }
+
 }
